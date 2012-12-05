4fa7d4203dc5712941a36c61c1324315702ffaf3
Roger Leite <roger.barreto@gmail.com>
2012-12-05 17:13:40 -0200

    Avoid over middleware instantiation

--- lib/http_monkey/configuration/middlewares.rb
+++ lib/http_monkey/configuration/middlewares.rb
@@ -5,23 +5,36 @@ module HttpMonkey
 
     def initialize
       @chain = []
+      @stack_middlewares = nil
     end
 
     def initialize_copy(source)
       super
       @chain = @chain.clone
+      @stack_middlewares = @stack_middlewares.clone if @stack_middlewares
     end
 
     def use(middleware, *args, &block)
       @chain << lambda { |app| middleware.new(app, *args, &block) }
+      @stack_middlewares = nil
       self
     end
 
     def execute(app, env)
-      stacked_middleware = @chain.reverse.inject(app) do |app, middle|
+      stacked = stack_middlewares(app, @chain)
+      stacked.call(env)
+    end
+
+    protected
+
+    def stack_middlewares(app, chain)
+      @stack_middlewares ||= stack_middlewares!(app, chain)
+    end
+
+    def stack_middlewares!(app, chain)
+      chain.reverse.inject(app) do |app, middle|
         middle.call(app)
       end
-      stacked_middleware.call(env)
     end
 
   end
