4980ad14350934d99b7fe3e06341b7174adfe0dd
Roger Leite <roger.barreto@gmail.com>
2012-12-26 16:06:07 -0200

    Removing old monkey patch

--- lib/http_monkey.rb
+++ lib/http_monkey.rb
@@ -14,6 +14,8 @@ require "http_monkey/client/environment_builder"
 require "http_monkey/client/response"
 require "http_monkey/middlewares"
 
+HTTPI.log = false
+
 module HttpMonkey
 
   def self.at(url)
@@ -42,15 +44,4 @@ module HttpMonkey
     end
   end
 
-end
-
-HTTPI.log = false
-
-# This monkey patch is to avoid a bug from httpi (1.1.1) on ruby 1.8.7
-# that raises "undefined method `use_ssl=' for Net::HTTP"
-if RUBY_VERSION =~ /1\.8/
-  class Net::HTTPSession
-    def use_ssl=(flag)
-    end
-  end
 end
